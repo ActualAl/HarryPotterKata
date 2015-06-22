c42abc14956d278cd93de9e1f2b479825bfd5fe5
Alex <alex.hardman@gmail.com>
2015-06-22 17:42:50 +0100

    removed unused using statements

--- HarryPotterKata.Unit.Tests/PriceTests.cs
+++ HarryPotterKata.Unit.Tests/PriceTests.cs
@@ -5,8 +5,6 @@ using FluentAssertions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HarryPotterKata.Unit.Tests
 {
